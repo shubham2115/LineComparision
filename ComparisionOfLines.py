3d8c268275912b0dc284322cad184ae144d395a4
Shubham <s.shrikhande1@gmail.com>
2022-02-22 00:33:55 +0530

    [shubham]:Added this file UC_3 line comparision

---
+++ ComparisionOfLines.py
@@ -0,0 +1,26 @@
+#Program to check wheather the lines are equal are not
+import math
+
+print("Enter Co-ordinates for A(x1,y1)")
+x1=int(input())
+y1=int(input())
+print("Enter Co-ordinates for B(x2,y2)")
+x2=int(input())
+y2=int(input())
+print("Enter Co-ordinates for C(x3,y3)")
+x3 = int(input())
+y3 = int(input())
+print("Enter Co-ordinates for B(x4,y4)")
+x4 = int(input())
+y4 = int(input())
+length1 = math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2) * 1.0)
+length2 = math.sqrt(math.pow(x3 - x4, 2) + math.pow(y4 - y3, 2) * 1.0)
+
+if length1==length2:
+    print("Both lines are equal")
+elif length1<length2:
+    print("first line is smaller\n"
+          "second line is greater")
+else:
+    print("second line is smaller\n"
+          "first line is greater")
